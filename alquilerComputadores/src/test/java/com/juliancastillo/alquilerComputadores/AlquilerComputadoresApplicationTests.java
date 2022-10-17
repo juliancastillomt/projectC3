1c8c25daaefa1e608c672c4d354e709a9dd372fd
juliancastillomt <julian.castillo.mt@usa.edu.co>
2022-10-16 20:35:28 -0500

    Agregar carpeta principal del proyecto

---
+++ alquilerComputadores/src/test/java/com/juliancastillo/alquilerComputadores/AlquilerComputadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliancastillo.alquilerComputadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerComputadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
